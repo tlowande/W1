c96a3e6667f6f10c59c13e7218c302cd5e51a882
Tamires <tamires.lowande@gmail.com>
2019-09-18 17:19:30 +0000

    counting over objects and middle refactored

--- lotide/middle.js
+++ lotide/middle.js
@@ -23,18 +23,20 @@ let assertArraysEqual = function (array1, array2) {
 
 
 let middle = function(array) {
-  let midNum = [];
+  // let midNum = [];
   if(array.length === 1 || array.length === 2){
-    return midNum;
+    return [];
   }else if(array.length % 2 !== 0){
     const midIndex = Math.floor(array.length / 2);
-    midNum.push(array[midIndex]);
+    // midNum.push(array[midIndex]);
+    return [array[midIndex]];
   }else if(array.length % 2 === 0){
     const midIndex = (array.length / 2) - 1
-    midNum.push(array[midIndex]);
-    midNum.push(array[midIndex +1]);
+    // midNum.push(array[midIndex]);
+    // midNum.push(array[midIndex +1]);
+    return [array[midIndex], array[midIndex +1]]
   }
-  return midNum;
+  // return midNum;
 };
 
-console.log(middle([2]));
+console.log(middle([2, 6, 6, 8, 8]));
