aea2082cd5cb25c2ed503170c95601716266a420
Tamires <tamires.lowande@gmail.com>
2019-09-23 17:10:06 +0000

    refactored tail function with @haphamo

--- lotide/tail.js
+++ lotide/tail.js
@@ -11,15 +11,6 @@ const tail = function(array) {
   return result;
 };
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs");
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
 
-const empty = [];
-tail(empty); // no need to capture the return value since we are not checking it
-assertEqual(empty.length, 3); // original array should still have 3 elements!
+module.exports = tail
